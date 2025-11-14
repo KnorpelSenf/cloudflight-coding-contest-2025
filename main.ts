75244f79ff791a4e51114df7f71be959cbd35035
KnorpelSenf <shtrog@gmail.com>
2025-11-14 15:12:43 +0000

    lvl3
    Co-authored-by: Karim Issa <e5kimol4ser@users.noreply.github.com>
    Co-authored-by: Yannik Eik / YaAlex <YouMe2@users.noreply.github.com>

--- main.ts
+++ main.ts
@@ -4,25 +4,49 @@ const path = Deno.args[0];
 
 const lines = h.file(path).lines().filter(Boolean);
 
-console.log(lines.map(solveProblem).unlines());
+console.log(lines.map(pacePyramid).unlines());
 
 // Main Problem Solver
 
-function solveProblem(problemInput: string): string {
-  const paces = problemInput.words().map(h.int);
-
-  const { pos, time } = paces.map(splitPace).reduce(
-    (acc, curr) => {
-      return {
-        pos: acc.pos + curr.pos,
-        time: acc.time + curr.time,
-      };
-    },
-    { pos: 0, time: 0 },
-  );
-
-  return [pos, time].map(String).unwords();
+function* generator(sign: number = 1) {
+  yield 5 * sign;
+  yield 4 * sign;
+  yield 3 * sign;
+  yield 2 * sign;
+  while (true) {
+    yield 1 * sign;
+  }
 }
+
+function pacePyramid(line: string) {
+  const [distance, time] = line.words().map(h.int);
+  const gen = generator(Math.sign(distance));
+  const acceleration = Array.from(
+    { length: Math.floor(Math.abs(distance) / 2) },
+    () => undefined,
+  ).map(() => gen.next().value);
+  const center = Math.abs(distance) % 2 === 1 ? [gen.next().value] : [];
+  const deceleration = acceleration.slice().reverse();
+
+  return [0, ...acceleration, ...center, ...deceleration, 0].map(String)
+    .unwords();
+}
+
+// function solveProblem2(problemInput: string): string {
+//   const paces = problemInput.words().map(h.int);
+
+//   const { pos, time } = paces.map(splitPace).reduce(
+//     (acc, curr) => {
+//       return {
+//         pos: acc.pos + curr.pos,
+//         time: acc.time + curr.time,
+//       };
+//     },
+//     { pos: 0, time: 0 },
+//   );
+
+//   return [pos, time].map(String).unwords();
+// }
 
 // Problem Helper
 
